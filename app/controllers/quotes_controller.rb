323317f65c0e2218e4a1974640e0c8013cfee434
Avery Pace <blainepace@gmail.com>
2021-07-10 04:58:07 -0400

    Add authenticate_user! to quotes controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,6 @@
 class QuotesController < ApplicationController
   before_action :set_quote, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /quotes or /quotes.json
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_070526) do
+ActiveRecord::Schema.define(version: 2021_07_10_085100) do
 
   create_table "quotes", force: :cascade do |t|
     t.text "body"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_07_10_070526) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
